9015fc4a007c0299f51d5fc3cdcff2e27f5f898f
Galisteoo00 <115107104+Galisteoo00@users.noreply.github.com>
2023-09-20 10:11:03 +0200

    Update ej1.php

--- ej1.php
+++ ej1.php
@@ -6,9 +6,8 @@
 </head>
 <body>
     <?php
-    // Código PHP
     echo '<p>¡Hola Mundo desde PHP!</p>';
     ?>
-    <a href="https://github.com/Galisteoo00/ej1-ud1-DWEC" target="_blank">Ver en GitHub</a>
+    <a href="https://github.com/Galisteoo00/ej1-ud1-DWEC">Ver en GitHub</a>
 </body>
 </html>
